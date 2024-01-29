46cdd3d8460f93245f6ea9c9d948beeb410e61cc
adrianpalacio <145379361+adrianpalacio@users.noreply.github.com>
2024-01-29 15:37:20 +0000

    Update SumaDeDosNumero.java

--- src/main/java/SumaDeDosNumero.java
+++ src/main/java/SumaDeDosNumero.java
@@ -1,2 +1,10 @@
-package PACKAGE_NAME;public class SumaDeDosNumero {
+public class SumaDeDosNumero {
+    public static void main (String[] args){
+        int numero1= 10;
+        int numero2= 20;
+
+        int suma = numero1 + numero3;
+
+        System.out.println ("La suma sería :" + suma);
+    }
 }
